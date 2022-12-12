894096a1afa847ac7fec06904c3407c7a0b49f86
sjenki69 <sjenki69@terpmail.umd.edu>
2022-12-12 15:00:44 -0500

    added

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-const Database = require('./Database')
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const Database = require('./Database');
 
-const db = new Database()
-db.connect()
+const db = new Database();
+db.connect();
 
-const app = express()
-app.set('views', path.resolve(__dirname, 'templates'))
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: false }))
+const app = express();
+app.set('views', path.resolve(__dirname, 'templates'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static('static'))
+app.use(express.static('static'));
